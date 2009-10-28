239146f7927197bd77488cb75c4428e6a2c7ef68
Julian Goacher <julian.goacher+github@gmail.com>
2009-10-28 21:41:16 +0000

    Added basic unit tests.

---
+++ test/core.js
@@ -0,0 +1,66 @@
+/** Core Nub MVC tests. */
+(function($) {
+
+    module('Nub: core');
+
+    test('Basic getting, setting and deleting', function() {
+        expect( 3 );
+        equals( $.nub.get('/data/test'), undefined, "/data/test is undefined");
+        $.nub.set('/data/test', 100 );
+        equals( $.nub.get('/data/test'), 100, "/data/test equals '100' after 'set' op");
+        $.nub.del('/data/test');
+        equals( $.nub.get('/data/test'), undefined, "/data/test is undefined after 'del' op");
+    });
+
+    test('Basic getting, setting and deleting with context path', function() {
+        expect( 3 );
+        var cx = '/data';
+        equals( $.nub.get('test', cx ), undefined, "/data/test is undefined");
+        $.nub.set('test', 100, cx );
+        equals( $.nub.get('test', cx ), 100, "/data/test equals '100' after 'set' op");
+        $.nub.del('test', cx );
+        equals( $.nub.get('test', cx ), undefined, "/data/test is undefined after 'del' op");
+    });
+
+    function initResult() { return { value: undefined, invoked: false }; }
+
+    test('Basic view functionality', function() {
+        expect( 6 );
+        var result = initResult();
+        var view = function() {
+            result.invoked = true;
+            result.value = $.nub.get('/data/test/value');
+        };
+        $.nub.view('/data/test/value', view );
+        // Test direct set.
+        $.nub.set('/data/test/value', 100 );
+        ok( result.invoked, "View invoked after '/data/test/value' set");
+        equals( result.value, 100, "View gets '100' from '/data/test/value'");
+        // Test parent set.
+        result = initResult();
+        $.nub.set('/data/test', { value:'abc' } );
+        ok( result.invoked, "View invoked after '/data/test' set");
+        equals( result.value, 'abc', "View reads 'abc' from '/data/test'");
+        // Test view isn't invoked when setting other parts of the model.
+        result = initResult();
+        $.nub.set('/data/other', 100 );
+        ok( !result.invoked, "View not invoked for update of non-observed value");
+        // Test view removal.
+        $.nub.removeView('/data/test/value', view );
+        result = initResult();
+        $.nub.set('/data/test','xyz');
+        ok( !result.invoked, "View not invoked after removal");
+    });
+
+    test('Multiple views', function() {
+        expect( 1 );
+        var result = [];
+        $.nub.view('/data/test/value', function() { result[0] = true; } );
+        $.nub.view('/data/test/value', function() { result[1] = true; } );
+        $.nub.view('/data/test', function() { result[2] = true; } );
+        $.nub.set('/data/test', { value: 23 } );
+        // Confirm that 'result' has 3 items, all true.
+        ok( result[0] && result[1] && result[2], "All views invoked after update");
+    });
+
+})(jQuery);
