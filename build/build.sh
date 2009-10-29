02f8afd6f447ed1e7f63e1c33216027672f126a2
Julian Goacher <julian.goacher+github@gmail.com>
2009-10-29 13:43:01 +0000

    Include unit tests in distribution archives.

--- build/build.sh
+++ build/build.sh
@@ -4,8 +4,11 @@ cp ../src/jquery.nub.js .
 cp ../src/jquery.nub-frames.js .
 ./minify.sh jquery.nub.js
 ./minify.sh jquery.nub-frames.js
-tar zcvf jquery.nub.$VER.tgz *js
-zip jquery.nub.$VER.zip *js
+mkdir test
+cp -r ../test/* test
+tar zcvf jquery.nub.$VER.tgz *js test/*
+zip jquery.nub.$VER.zip *js test/*
 mv *.js ../dist
 mv jquery.nub.$VER.tgz ../dist
 mv jquery.nub.$VER.zip ../dist
+rm -Rf test
